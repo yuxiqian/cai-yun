7db5ac9f9a477c4290d722201337e233ab87c6cd
´▽｀ <34335406+yuxiqian@users.noreply.github.com>
2018-10-01 09:56:26 +0800

    更新 Swift 项目。

---
+++ swift/cai-yun/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  cai-yun
+//
+//  Created by 法好 on 2018/9/30.
+//  Copyright © 2018 yuxiqian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true;
+    }
+
+}
+
---
+++ swift/cai-yun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cai-yun
+//
+//  Created by yuxiqian on 2018/9/30.
+//  Copyright © 2018 yuxiqian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
